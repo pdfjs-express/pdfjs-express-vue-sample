04c66f8640ffdbbe3f674161e54687d75ee592d8
phenethylamine <dustin.riley@me.com>
2020-04-17 17:19:55 -0700

    pdfjs express vue sample

---
+++ src/components/WebViewer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref='viewer'></div>
+</template>
+
+<script>
+/* eslint-disable */
+import WebViewer from '@pdftron/pdfjs-express'
+
+export default {
+  name: 'WebViewer',
+  props: {
+    path: String,
+    url: String
+  },
+  mounted: function () {
+        WebViewer({
+            path: this.path,
+            initialDoc: this.url, // replace with your own PDF file
+          }, this.$refs.viewer).then((instance) => {
+            // call apis here
+          });
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  width: 100%; 
+  height: 100vh;
+}
+</style>
